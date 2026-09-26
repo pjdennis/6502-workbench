792e2dbac3888133715645eb1760c2351c564387
Phil Dennis <pjdennis@gmail.com>
2026-09-26 13:46:39 -0500

    Remove hardcoded ports from michael and wendy upload scripts

--- tools/upload/compile_and_upload_michael.sh
+++ tools/upload/compile_and_upload_michael.sh
@@ -2,4 +2,4 @@
 # Usage: compile_and_upload_michael.sh <program.s>   (writes a.out in the current directory)
 HERE="$(cd "$(dirname "$0")" && pwd)"
 
-"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --port=/dev/cu.SLAB_USBtoUART --baudrate=57600 a.out
+"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --baudrate=57600 a.out
--- tools/upload/compile_and_upload_wendy.sh
+++ tools/upload/compile_and_upload_wendy.sh
@@ -2,4 +2,4 @@
 # Usage: compile_and_upload_wendy.sh <program.s>   (writes a.out in the current directory)
 HERE="$(cd "$(dirname "$0")" && pwd)"
 
-"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --noreset --port=/dev/cu.usbserial-1420 --baudrate=115200 a.out
+"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --noreset --baudrate=115200 a.out
