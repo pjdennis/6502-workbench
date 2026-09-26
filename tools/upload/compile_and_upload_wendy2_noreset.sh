fba434aeeb1b7f8cc4da9ab43f7b8cc602e381dc
Phil Dennis <pjdennis@gmail.com>
2026-09-26 13:39:58 -0500

    The 'no reset' version of the wendy2 upload script now uses automatic port detection

--- tools/upload/compile_and_upload_wendy2_noreset.sh
+++ tools/upload/compile_and_upload_wendy2_noreset.sh
@@ -2,4 +2,4 @@
 # Usage: compile_and_upload_wendy2_noreset.sh <program.s>   (writes a.out in the current directory)
 HERE="$(cd "$(dirname "$0")" && pwd)"
 
-"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --port=/dev/cu.SLAB_USBtoUART --baudrate=115200 --noreset a.out
+"$HERE/../../firmware/vasm" -quiet -wdc02 -wfail -Fbin -dotdir -ignore-mult-inc -esc $1 && python3 "$HERE/transfer.py" --baudrate=115200 --noreset a.out
